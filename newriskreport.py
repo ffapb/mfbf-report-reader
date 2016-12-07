72d05415059a2ac7830ebccd014d92ffbd68d21c
Shadi Akiki <shadiakiki1986@gmail.com>
2016-12-07 15:41:34 +0200

    convert to dictionary

--- newriskreport.py
+++ newriskreport.py
@@ -1,9 +1,12 @@
 import csv
 
+data={}
 with open('./newrisk.csv') as csvfile:
 	reader=csv.reader(csvfile,delimiter=',')
 	for row in reader:
-		print(row[31],row[42],row[43])
+		data[row[31]]={'initial':row[42],'maintenance':row[43]}
+
+print data
 
 		
 '''ABC123 76678.05 76678.05
