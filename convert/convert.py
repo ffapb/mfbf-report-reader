804cd89b8b964dcde3d4e5b9b2c98d40949cb19b
FFAI\mmoawad <minerva.moawad@gmail.com>
2016-12-09 16:56:33 +0200

    modify convert.py

--- convert/convert.py
+++ convert/convert.py
@@ -2,10 +2,10 @@ import csv
 import yaml
 
 data={}
-with open('./newrisk.csv') as csvfile:
+with open('./accounts.csv') as csvfile:
 	reader=csv.reader(csvfile,delimiter=',')
 	for row in reader:
 		data[row[31]]={'initial':row[42],'maintenance':row[43]}
 
-with open('newrisk.yml', 'w') as outfile:
+with open('accounts.yml', 'w') as outfile:
     yaml.dump(data, outfile, default_flow_style=False)
